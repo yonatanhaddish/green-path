a5017cdbf2da16ede1f06bb5f724998e29dda467
Yonatan Haddish <yonihailshn@gmail.com>
2025-02-14 18:45:33 -0500

    frontend page template structured

---
+++ frontend/app/job-board/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const JobBoard = () => {
+  return (
+    <>
+      <div>Job Board</div>
+    </>
+  );
+};
+export default JobBoard;
---
+++ frontend/app/job-form/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobForm = () => {
+  return <div>Job Form</div>;
+};
+
+export default JobForm;
