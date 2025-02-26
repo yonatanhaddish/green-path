044aa874c35b3f72af42b05cb5ba1fe3556b0193
Yonatan Haddish <yonihailshn@gmail.com>
2025-02-25 21:02:44 -0500

    job-post page structured and in-progress

--- frontend/app/job-form/page.js
+++ frontend/app/job-form/page.js
@@ -1,7 +1,71 @@
-import React from "react";
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DateField } from "@mui/x-date-pickers/DateField";
+import { Typography } from "@mui/material";
 
-const JobForm = () => {
-  return <div>Job Form</div>;
-};
-
-export default JobForm;
+export default function JobForm() {
+  const styles = {
+    cardContent: {
+      border: "solid red 2px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "30px",
+    },
+  };
+  return (
+    <Box component="form" noValidate autoComplete="off" sx={styles.cardContent}>
+      <Box>
+        <Typography>Job Id: #123123</Typography>
+        <Typography>Eagle Import & Export</Typography>
+      </Box>
+      <Box>
+        <Box>
+          <TextField
+            required
+            id="pickup-location"
+            label="Pickup Location"
+            defaultValue="123 Main St."
+            fullWidth
+          />
+          <TextField
+            required
+            id="dropoff-location"
+            label="Dropoff Location"
+            defaultValue="100 Wellington St."
+            fullWidth
+          />
+        </Box>
+        <Box>
+          <TextField
+            id="distance"
+            label="Distance (km)"
+            type="number"
+            fullWidth
+          />
+          <TextField id="weight" label="Weight (kg)" type="number" fullWidth />
+        </Box>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Box>
+            <DemoContainer components={["DateField"]}>
+              <DateField label="Delivery Date" fullWidth />
+            </DemoContainer>
+          </Box>
+          <Box>
+            <DemoContainer components={["DateField"]}>
+              <DateField label="Delivery Date" fullWidth />
+            </DemoContainer>
+          </Box>
+        </LocalizationProvider>
+        <Box>
+          <TextField id="price" label="Price ($)" type="number" fullWidth />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- frontend/app/operator-my-jobs/page.js
+++ frontend/app/operator-my-jobs/page.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AcceptedJobs from "../components/AcceptedJobs";
+import RecentActivity from "../components/RecentActivity";
 import { Box } from "@mui/material";
 const MyJobs = () => {
   const styles = {
     cardParent: {
-      border: "solid blue 2px",
+      // border: "solid red 2px",
+    },
+    cardAcceptedJobs: {
+      // border: "solid blue 2px",
       width: "100%",
       display: "flex",
       flexDirection: "row",
@@ -14,18 +18,29 @@ const MyJobs = () => {
       paddingTop: "80px",
       paddingBottom: "100px",
     },
+    recentlyActivity: {
+      // border: "solid green 2px",
+      width: "90%",
+      display: "flex",
+      justifyContent: "center",
+    },
   };
 
   return (
     <Box sx={styles.cardParent}>
-      <AcceptedJobs />
-      <AcceptedJobs />
-      <AcceptedJobs />
-      <AcceptedJobs />
-      <AcceptedJobs />
-      <AcceptedJobs />
-      <AcceptedJobs />
-      <AcceptedJobs />
+      <Box sx={styles.cardAcceptedJobs}>
+        <AcceptedJobs />
+        <AcceptedJobs />
+        <AcceptedJobs />
+        <AcceptedJobs />
+        <AcceptedJobs />
+        <AcceptedJobs />
+        <AcceptedJobs />
+        <AcceptedJobs />
+      </Box>
+      <Box sx={styles.recentlyActivity}>
+        <RecentActivity />
+      </Box>
     </Box>
   );
 };
