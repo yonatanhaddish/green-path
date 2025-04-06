5cfe0bfa0ac97aa91a25d505617a06a2aa183e16
Yonatan Haddish <yonihailshn@gmail.com>
2025-04-05 20:52:38 -0400

    green path fronend almost done, remainng some refactoring' '

--- frontend/app/components/ContactUsSection.js
+++ frontend/app/components/ContactUsSection.js
@@ -17,6 +17,24 @@ function ContactUsSection() {
   const screenLessThan430 = useMediaQuery(
     "(min-width: 100px) and (max-width: 430px)"
   );
+  const screenGreaterThan430LessThan768 = useMediaQuery(
+    "(min-width: 431px) and (max-width: 768px)"
+  );
+  const screenGreaterThan768LessThan1024 = useMediaQuery(
+    "(min-width: 769px) and (max-width: 1024px)"
+  );
+  const screenGreaterThan1024LessThan3840 = useMediaQuery(
+    "(min-width: 1025px) and (max-width: 3840px)"
+  );
+  const screenGreaterThan1280LessThan1440 = useMediaQuery(
+    "(min-width: 1281px) and (max-width: 1440px)"
+  );
+  const screenGreaterThan1440LessThan1920 = useMediaQuery(
+    "(min-width: 1441px) and (max-width: 1920px)"
+  );
+  const screenGreaterThan1920LessThan2560 = useMediaQuery(
+    "(min-width: 1921px) and (max-width: 2560px)"
+  );
   useEffect(() => {
     setLoading(false);
   }, []);
@@ -43,19 +61,33 @@ function ContactUsSection() {
       boxShadow: "0 0 10px green",
       display: "flex",
       flexDirection: "column",
-      gap: "50px",
+      gap: "30px",
       paddingBottom: "20px",
       paddingTop: "30px",
-      width: "80%",
+      width: screenGreaterThan1280LessThan1440
+        ? "70%"
+        : screenGreaterThan1440LessThan1920
+        ? "60%"
+        : screenGreaterThan1920LessThan2560
+        ? "50%"
+        : "80%",
       margin: "0 auto",
-      marginBottom: "100px",
+      marginBottom: "60px",
     },
     input_box_parent: {
       // border: "solid blue 2px",
       display: "flex",
       flexDirection: "column",
       gap: "15px",
-      width: "90%",
+      width: screenLessThan430
+        ? "90%"
+        : screenGreaterThan430LessThan768
+        ? "80%"
+        : screenGreaterThan768LessThan1024
+        ? "55%"
+        : screenGreaterThan1024LessThan3840
+        ? "40%"
+        : "30%",
       margin: "0 auto",
     },
     box_button: {
@@ -95,69 +127,119 @@ function ContactUsSection() {
     typo_desc: {
       fontFamily: "italic",
     },
+    info_box: {
+      border: "solid green 1px",
+      boxShadow: "0 0 10px green",
+      color: "#fff",
+      backgroundColor: "#000",
+      width: screenLessThan430
+        ? "90%"
+        : screenGreaterThan430LessThan768
+        ? "80%"
+        : screenGreaterThan768LessThan1024
+        ? "55%"
+        : screenGreaterThan1024LessThan3840
+        ? "40%"
+        : "30%",
+      display: "flex",
+      flexDirection: "column",
+      padding: "10px",
+      height: screenGreaterThan1024LessThan3840 ? "300px" : "100%",
+      alignSelf:
+        screenLessThan430 ||
+        screenGreaterThan768LessThan1024 ||
+        screenGreaterThan430LessThan768
+          ? "center"
+          : "",
+      marginLeft: screenGreaterThan1024LessThan3840 ? "30px" : "",
+    },
+    info_input_box: {
+      // border: "solid purple 2px",
+      display: "flex",
+      flexDirection: screenGreaterThan1024LessThan3840 ? "row" : "column",
+      gap: "10px",
+      // alignSelf: screenGreaterThan1024LessThan3840 ? "center" : "",
+      alignItems: screenGreaterThan1024LessThan3840 ? "center" : "",
+    },
+    typo_info: {
+      fontSize: screenGreaterThan1024LessThan3840 ? "22px" : "16px",
+    },
   };
   return (
     <>
       <Box sx={styles.box_parent}>
         <Typography sx={styles.typo_heading}>Contact Us</Typography>
-        <Box
-          sx={styles.input_box_parent}
-          component="form"
-          noValidate
-          autoComplete="off"
-        >
-          <Typography sx={styles.typo_desc}>
-            Feel free to contact us any time. We will get back to your as soon
-            as we can!
-          </Typography>
-          <TextField
-            sx={styles.input_field}
-            id="full-name"
-            label="Full Name"
-            variant="outlined"
-            size="small"
-            fullWidth
-          />
-          <TextField
-            sx={styles.input_field}
-            id="email"
-            label="Email"
-            variant="outlined"
-            size="small"
-            fullWidth
-          />
-          <FormControl fullWidth sx={styles.input_field}>
-            <InputLabel id="demo-simple-select-label">
-              Choose Service Type
-            </InputLabel>
-            <Select
+        <Box sx={styles.info_input_box}>
+          <Box sx={styles.info_box}>
+            <Typography sx={styles.typo_info}>Info</Typography>
+            <Typography sx={styles.typo_info}>
+              📧 greenpath@email.com
+            </Typography>
+            <Typography sx={styles.typo_info}>📞 (437) 123 1212</Typography>
+            <Typography sx={styles.typo_info}>📍 123 Main St. E</Typography>
+            <Typography sx={styles.typo_info}>
+              ⏰ 09:00 AM - 07:00 PM
+            </Typography>
+          </Box>
+          <Box
+            sx={styles.input_box_parent}
+            component="form"
+            noValidate
+            autoComplete="off"
+          >
+            <Typography sx={styles.typo_desc}>
+              Feel free to contact us any time. We will get back to your as soon
+              as we can!
+            </Typography>
+            <TextField
+              sx={styles.input_field}
+              id="full-name"
+              label="Full Name"
+              variant="outlined"
               size="small"
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value="load_owner"
-              label="Select One"
-              //   onChange={handleChange}
-            >
-              <MenuItem value="load_owner">Load Owner</MenuItem>
-              <MenuItem value="van_owner">Van Owner</MenuItem>
-            </Select>
-          </FormControl>
-          <TextField
-            sx={styles.input_field}
-            id="message"
-            label="Message"
-            variant="outlined"
-            fullWidth
-            multiline
-            size="small"
-            minRows={8}
-            maxRows={10}
-            // onChange={handleChangePhoneNumberValue}
-          />
-          {/* <Box sx={styles.box_button}>
+              fullWidth
+            />
+            <TextField
+              sx={styles.input_field}
+              id="email"
+              label="Email"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+            <FormControl fullWidth sx={styles.input_field}>
+              <InputLabel id="demo-simple-select-label">
+                Choose Service Type
+              </InputLabel>
+              <Select
+                size="small"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value="load_owner"
+                label="Select One"
+                //   onChange={handleChange}
+              >
+                <MenuItem value="load_owner">Load Owner</MenuItem>
+                <MenuItem value="van_owner">Van Owner</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              sx={styles.input_field}
+              id="message"
+              label="Message"
+              variant="outlined"
+              fullWidth
+              multiline
+              size="small"
+              minRows={8}
+              maxRows={10}
+              // onChange={handleChangePhoneNumberValue}
+            />
+            {/* <Box sx={styles.box_button}>
            
           </Box> */}
-          <Button sx={styles.box_button}>Send Message</Button>
+            <Button sx={styles.box_button}>Send Message</Button>
+          </Box>
         </Box>
       </Box>
     </>
